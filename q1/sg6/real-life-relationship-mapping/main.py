1daacd343373142388204e009aadea0f3776bd51
Castor Troy <cmquimno@zrc.pshs.edu.ph>
2026-09-20 05:32:22 +0800

    Implement relationship mapping in main.py

--- q1/sg6/real-life-relationship-mapping/main.py
+++ q1/sg6/real-life-relationship-mapping/main.py
@@ -1,1 +1,12 @@
-# Write a short Python code snippet showing a Course adding a Student object to a list. 
+class Student:
+    def __init__(self, name: str, student_id: str):
+        self._name = name
+        self._student_id = student_id
+
+class Course:
+    def __init__(self, course_name: str):
+        self._course_name = course_name
+        self._students = [] 
+
+    def add_student(self, student: Student) -> None:
+        self._students.append(student)# Write a short Python code snippet showing a Course adding a Student object to a list. 
